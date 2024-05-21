83af4a271e9a2d25547e50f20bd45a1ce4620a13
jeMartine <jorgeesteban464@gmail.com>
2024-05-21 17:46:08 -0500

    sistema de calidad Edge y fog

---
+++ fog/scfog.py
@@ -0,0 +1,21 @@
+import datetime
+import dotenv
+import zmq
+
+dotenv.load_dotenv()
+
+def generate_response(content):
+    return {
+        'content': content,
+        'timestamp': datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    }
+
+context = zmq.Context()
+
+qualitySystem = context.socket(zmq.REP)
+qualitySystem.bind("tcp://*:5558")
+
+while True:
+    alert = qualitySystem.recv()
+    qualitySystem.send_json(generate_response("ok"))
+    print("SISTEMA DE CALIDAD FOG: ¡Se recibió una alerta de humo!")
