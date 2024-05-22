5d21691d22c5736c48c28b93bb0c6ed24b1c32e0
Valentina Garcia <valentinagarciaalfonso@gmail.com>
2024-05-21 22:49:42 -0500

    Refactorización

--- fog/fog.js
+++ fog/fog.js
@@ -4,45 +4,29 @@ const config = require("./config");
 (async function() {
     const fogReceiver = new zmq.Pull();
     fogReceiver.bind(`tcp://${config.fogLayer.ip}:${config.fogLayer.port}`);
-    console.log(`Capa fog - puerto ${config.fogLayer.port}`);
+    console.log(`Capa fog escuchando en puerto ${config.fogLayer.port}`);
 
     const cloudSender = new zmq.Push();
     cloudSender.connect(`tcp://${config.cloudLayer.ip}:${config.cloudLayer.port}`);
     console.log("Capa fog conectada a capa cloud");
 
-    function processTemperatureData(data) {
-        const temperatures = data.map(d => d.value);
-        const averageTemp = temperatures.reduce((acc, temp) => acc + temp, 0) / temperatures.length;
-        console.log(`Temperatura promedio: ${averageTemp.toFixed(2)}°C`);
+    let humidityData = [];
 
-        if (averageTemp > 29.4) {
-            console.log('La temperatura excede el límite, enviando alerta...');
-            cloudSender.send(JSON.stringify({ alert: 'Temperatura excede el límite' }));
-        }
-    }
-
-    function processHumidityData(data) {
-        const humidities = data.map(d => d.value);
-        const averageHumidity = humidities.reduce((acc, humidity) => acc + humidity, 0) / humidities.length;
+    const calculateAverageHumidity = () => {
+        if (humidityData.length === 0) return;
+        const totalHumidity = humidityData.reduce((acc, reading) => acc + reading.value, 0);
+        const averageHumidity = totalHumidity / humidityData.length;
         console.log(`Humedad promedio: ${averageHumidity.toFixed(2)}%`);
+        cloudSender.send(JSON.stringify({ type: 'humidity', average: averageHumidity, timestamp: new Date() }));
+        humidityData = [];
+    };
 
-        if (averageHumidity < 70) {
-            console.log('Humedad por debajo del límite, enviando alerta');
-            cloudSender.send(JSON.stringify({ alert: 'Humedad por debajo del límite' }));
-        }
-
-        cloudSender.send(JSON.stringify({ type: 'humidity', values: data }));
-    }
+    setInterval(calculateAverageHumidity, 5000); // Calcular cada 5 segundos
 
     for await (const [msg] of fogReceiver) {
         const sensorData = JSON.parse(msg.toString());
-
-        if (sensorData.type === 'temperature') {
-            processTemperatureData(sensorData.values);
-        } else if (sensorData.type === 'humidity') {
-            processHumidityData(sensorData.values);
-        } else if (sensorData.type === 'smoke') {
-            console.log(`Datos de sensor de humo recibidos: ${JSON.stringify(sensorData.values)}`);
+        if (sensorData.type === 'humidity') {
+            humidityData.push(...sensorData.values);
         }
     }
 })();
